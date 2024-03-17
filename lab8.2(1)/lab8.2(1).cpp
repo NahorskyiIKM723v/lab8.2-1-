d1926c83786752a992f1907ebaa0b7d509ea16b3
igor <igor@DESKTOP-EH4IM96>
2024-03-18 00:52:26 +0200

    Добавьте файлы проекта.

---
+++ lab8.2(1)/lab8.2(1).cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_CTYPE, "ukr");
+    int n;
+    cout << "Введіть розмір масиву: ";
+    cin >> n;
+
+    double a, b;
+    cout << "Введіть межі діапазону (a,b): ";
+    cin >> a;
+    cin >> b;
+
+    int* arr = new int[n];
+    cout << "Введіть елементи масиву: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] >= a && arr[i] <= b) {
+            count++;
+        }
+    }
+    double max = arr[0];
+    int maxIndex = 0;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+            maxIndex = i;
+        }
+    }
+    double sum = 0;
+    for (int i = maxIndex + 1; i < n; i++) {
+        sum += arr[i];
+    }
+    cout << "Кількість елементів у діапазоні [" << a << ", " << b << "]: " << count << endl;
+    cout << "Сума елементів після максимального: " << sum << endl;
+}
